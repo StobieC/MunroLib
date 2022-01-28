f5c4a5cf4682d18c6df6a5084fbc0530949e3607
cameron.stobie <cameron.stobie@rakuten.com>
2022-01-28 16:08:20 +0900

    seperate csv reading logic

--- munro-lib/src/main/java/com/example/munro/csvclient/CSVReader.kt
+++ munro-lib/src/main/java/com/example/munro/csvclient/CSVReader.kt
@@ -10,30 +10,23 @@ class CSVReader {
     companion object {
         @Throws(FileNotFoundException::class, IOException::class)
         fun readCSV(): MutableList<MunroResult> {
-            val munroResults = mutableListOf<MunroResult>()
-
             val br = BufferedReader(StringReader(bigOlCsv))
-
-            br.forEachLine {
-                val fields = it.split(",".toRegex()).toTypedArray()
-                val name            = fields[5]
-                val heightMetre     = fields[9]
-                val gridRef         = fields[13]
-                val postNineteen97  = munType(fields[27])
-
-                munroResults.add(MunroResult(name, heightMetre.toDouble(), postNineteen97, gridRef))
-            }
-
-            br.close()
-            return munroResults
+            return munList(br)
         }
 
         fun readCSV(file: BufferedInputStream): MutableList<MunroResult> {
-            val munroResults = mutableListOf<MunroResult>()
-
             val br = file.bufferedReader()
             //skip first line
             br.readLine()
+            return munList(br)
+        }
+
+        private fun munType(munString: String): MunType {
+            return if (munString == "MUN") { MunType.MUN } else if (munString == "TOP") { MunType.TOP} else { MunType.BLANK}
+        }
+
+        private fun munList(br: BufferedReader): MutableList<MunroResult> {
+            val munroResults = mutableListOf<MunroResult>()
             br.forEachLine {
                 val fields = it.split(",".toRegex()).toTypedArray()
 
@@ -50,10 +43,6 @@ class CSVReader {
 
             br.close()
             return munroResults
-        }
-
-        private fun munType(munString: String): MunType {
-            return if (munString == "MUN") { MunType.MUN } else if (munString == "TOP") { MunType.TOP} else { MunType.BLANK}
         }
 
     }
